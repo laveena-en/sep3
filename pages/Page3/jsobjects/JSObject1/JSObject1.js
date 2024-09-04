b74544ff3a1e0a133bc2db61d63378836b321268
Laveena Enid <laveena@appsmith.com>
2024-09-04 05:23:38 +0000

    Page3 added Page4 added

---
+++ pages/Page3/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,11 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		showAlert("Test");
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
